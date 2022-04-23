865054cbdefecde3f86e886a87dc57cdcdc9ad61
Alex <alexanderbergquist80@gmail.com>
2022-04-23 19:36:48 +0200

    get all films from API in FilmsList

--- src/pages/FilmsList.jsx
+++ src/pages/FilmsList.jsx
@@ -1,6 +1,43 @@
+// React & Bootstrap
+import { useState, useEffect } from 'react'
+// import { useParams } from 'react-router-dom'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
+
+// Imports
+import SWAPI from '../services/SWAPI'
+
 
 export default function FilmsList() {
+  const [films, setFilms] = useState([])
+
+  const getFilms = async () => {
+    const data = await SWAPI.getAllFilms()
+    setFilms(data.results)
+  }
+
+  useEffect(() => {
+    getFilms()
+  }, [])
+
+
   return (
-    <div>FilmsList</div>
+
+    <>
+      <h1>Films</h1>
+
+      <ListGroup className="filmslist">
+        {films.map(film => 
+            <ListGroup.Item key={film.id}>
+              <h3>{film.title}</h3>
+              <p>Episode: {film.episode_id}</p>
+              <p>Relase date: {film.release_date}</p>
+              <p>Characters: {film.characters.length}</p>
+              <Button>Read more</Button>
+            </ListGroup.Item>
+          )}
+
+        </ListGroup>
+    </>
   )
 }
